48450a3938a8e2f602dd8e1474b24b9e8c018534
bayuyusufm <bayuyusufm@gmail.com>
2026-07-12 15:26:43 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btn = document.querySelector("button");
+
+    btn.addEventListener("click", function () {
+        alert("Selamat Datang di Undangan Pernikahan Bayu Yusuf Maulana & Sarnila ❤️");
+    });
+});
